8e3441c766f68e8ac9683f96c92044e6b00682a6
shabrat2015 <shabrat2015@gmail.com>
2021-01-03 17:04:04 +0200

    2Ex

--- src/main/java/org/itstep/step02/Pair.java
+++ src/main/java/org/itstep/step02/Pair.java
@@ -1,3 +1,21 @@
 package org.itstep.step02;
 
-// TODO: Реализуйте здесь обобщенный класс Pari
+public class Pair<One,Two>
+{
+    private final One oneElement;
+    private final Two twoElement;
+
+    public Pair(One oneElement, Two twoElement) {
+        this.oneElement = oneElement;
+        this.twoElement = twoElement;
+    }
+
+    public One getFirst() {
+        return oneElement;
+    }
+
+    public Two getSecond() {
+        return twoElement;
+    }
+
+}

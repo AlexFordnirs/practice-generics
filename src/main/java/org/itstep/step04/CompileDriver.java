975f21f17b19967a12fb9e305bf045d2526faba1
shabrat2015 <shabrat2015@gmail.com>
2021-01-03 18:33:27 +0200

    4XX

--- src/main/java/org/itstep/step04/CompileDriver.java
+++ src/main/java/org/itstep/step04/CompileDriver.java
@@ -19,27 +19,27 @@ public class CompileDriver {
         Pair<?, ?> p4;
         Pair<?, ? extends Number> p5;
 
-        p1 = new Pair<String, Integer>("A", 7);
-        int a = p1.getSecond();
+        p1 = new Pair<String, Integer>("A", 7); // соответсвие типов
+        int a = p1.getSecond();//да
 
-       // p1 = new Pair<Integer, Double>(23, 12.0);
+      //  p1 = new Pair<Integer, Double>(23, 12.0);//Неверный формат вводимых данных
 
-        //p2 = new Pair<String, Integer>("B", 8);
+       // p2 = new Pair<String, Integer>("B", 8);//Неверный формат вводимых данных
 
-        //p3 = new Pair<String, Integer>("C", 9);
+      //  p3 = new Pair<String, Integer>("C", 9);//Неверный формат вводимых данных
 
-        p4 = new Pair<String, String>("House", "Car");
-        p4 = new Pair<String, Integer>("D", 10);
+        p4 = new Pair<String, String>("House", "Car");//ДА соответсвие типов
+        p4 = new Pair<String, Integer>("D", 10);//ДА соответсвие типов
 
-        //Integer b = p4.getSecond();
-        Integer c = (Integer) p4.getSecond();
+      //  Integer b = p4.getSecond();// при попытке захвата  даеться ошибка по причине того, что не все Object можно запихнувть в Integer b
+        Integer c = (Integer) p4.getSecond();//А сдесь по сути альтернатива Convet.ToInt поэтому программа и не ругаеться
 
-        //p5 = new Pair<String, String>("E", "G");
-        p5 = new Pair<String, Double>("E", 11.0);
-        p5 = new Pair<String, Integer>("E", 11);
+      //  p5 = new Pair<String, String>("E", "G");//ошибка из-за попутки записать не число в число
+        p5 = new Pair<String, Double>("E", 11.0);//ДА соответсвие типов
+        p5 = new Pair<String, Integer>("E", 11);//ДА соответсвие типов
 
-       // Integer d = p5.getSecond();
-        Integer e = (Integer) p5.getSecond();
+      //  Integer d = p5.getSecond();//ошабка из-за попытка захвата в Integer d , Number что может означатьа, что сумеп Number неудаеться конверитрывать в Integer "that are convertible to the primitive types byte, double, float, int, long, and short."
+        Integer e = (Integer) p5.getSecond();//да//А сдесь по сути альтернатива Convet.ToInt поэтому программа и не ругаеться
     }
 
 }
